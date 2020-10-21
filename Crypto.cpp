8d0b507fb926677661deee2c393fd119f1711456
ychen65 <60162710+ychen65@users.noreply.github.com>
2020-10-21 15:12:48 -0400

    Update Crypto.cpp

--- Crypto.cpp
+++ Crypto.cpp
@@ -2,8 +2,8 @@
 //  Crypto.cpp
 //  CS331_HW04
 //
-//  Created by Chenyu Wang on 11/14/19.
-//  Copyright © 2019 chenyu wang. All rights reserved.
+//  Created by Yongqiang Chen on 11/14/19.
+//  Copyright © 2019 Yongqiang Chen. All rights reserved.
 //
 
 #include "Crypto.h"
